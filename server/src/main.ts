5bef6e7be783a91c5528d69ea196524703ff7b70
Mehmet Ozan Turhan <m.ozanturhan@hotmail.com>
2021-04-18 13:42:37 +0300

    add error handling for redis and cors options

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { IoAdapter } from '@nestjs/platform-socket.io';
 import * as redisIoAdapter from 'socket.io-redis';
+import * as cors from 'cors';
 import * as helmet from 'helmet';
 
 export class RedisIoAdapter extends IoAdapter {
@@ -13,24 +14,35 @@ export class RedisIoAdapter extends IoAdapter {
       port: process.env.REDIS_PORT,
       auth_pass: process.env.REDIS_PASSWORD,
     });
+
+    redisAdapter.prototype.on('error', function (err) {
+      console.error('adapter error: ', err);
+    });
+
     server.adapter(redisAdapter);
     return server;
   }
 }
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    cors: {
-      origin:
-        process.env.REDIS_HOST === 'localhost'
-          ? '*'
-          : 'https://react-socket-io-webrtc-client.herokuapp.com',
-    },
-  });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const whitelist = ['https://react-socket-io-webrtc-client.herokuapp.com/'];
+
+  const corsOptionsDelegate = function (req, callback) {
+    let corsOptions;
+    if (whitelist.indexOf(req.header('Origin')) !== -1) {
+      corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+    } else {
+      corsOptions = { origin: false }; // disable CORS for this request
+    }
+    callback(null, corsOptions); // callback expects two parameters: error and options
+  };
+
+  app.use(cors(corsOptionsDelegate));
   app.use(helmet());
   app.useWebSocketAdapter(new RedisIoAdapter(app));
 
-  console.log('Port', process.env.PORT);
   await app.listen(process.env.PORT || 3000);
 }
 
